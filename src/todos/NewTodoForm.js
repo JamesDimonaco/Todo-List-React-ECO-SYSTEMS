bdcd07f87a9e507e0dd7a0389659f4113b03d2ce
james dimonaco <dimonaco.james@gmail.com>
2020-10-13 14:42:20 +0100

    first commit

---
+++ src/todos/NewTodoForm.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import './NewTodoForm.css';
+
+
+
+
+const NewTodoForm = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    return (
+        <div className="new-todo-form">
+            <input
+                className="new-todo-input"
+                type="text"
+                placeholder="Type your new todo here"
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)} />
+            <button className="new-todo-button">Create Todo</button>
+        </div>
+    );
+};
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
