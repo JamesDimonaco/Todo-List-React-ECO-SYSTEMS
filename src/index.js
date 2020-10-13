977225ca61ad6fd6d1b7dad2f56cb983d5222e65
james dimonaco <dimonaco.james@gmail.com>
2020-10-13 19:35:39 +0100

    adding redux

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react'
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux'; 
 import { configureStore } from './store'
+import { persistStore } from 'redux-persist';
+import { persistGate } from 'redux-persist/lib/integration/react'
 import App from './App.js'
 
+
+const store = configureStore();
+const persistor = persistStore(store)
+
 ReactDom.render(
-<Provider store={configureStore()}>
-     <App /> 
+<Provider store={store}>
+     <persistGate loading={<div>Loading.....</div>} persistor={persistor}> 
+          <App /> 
+     </persistGate>
+    
 </Provider>,
 
-document.getElementById('root'));
+document.getElementById('root'),
+);
